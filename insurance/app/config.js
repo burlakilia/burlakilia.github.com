773c37a095f7c194f9a30c0c9e389b058b254b2f
Burlak Ilia <i.burlak@it-online.ru>
2013-05-11 11:32:09 +0400

    add config file

---
+++ insurance/app/config.js
@@ -0,0 +1,17 @@
+define(function () {
+    "use strict";
+
+    // Use strict JSON format
+    return {
+
+        "data":  {
+            "url": "/insurance/data.json"
+        },
+
+        "locations": {
+            "url": "/insurance/locations.json"
+        }
+
+    }
+
+});
