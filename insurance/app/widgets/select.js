bc2d9733c22a66b5a97a1c128badb7a41023b3fb
Burlak Ilia <i.burlak@it-online.ru>
2013-05-24 13:14:35 +0400

    fix ie7 error

--- insurance/app/widgets/select.js
+++ insurance/app/widgets/select.js
@@ -91,12 +91,6 @@ define(function (require, exports) {
 
         $(document).click(check);
 
-        document.addEventListener('touchend', function(e) {
-            e.preventDefault();
-            var touch = e.touches[0];
-            console.log(touch.pageX + " - " + touch.pageY, e);
-        }, false);
-
         container.on('click', '.item', onclick);
     };
 
