f3485ef16d03a979591d15c819fbb2995fe24647
Burlak Ilia <i.l.burlak@gmail.com>
2014-12-24 11:25:42 +0300

    актуализированы статусы домов

--- selenskie/public/data/status.js
+++ selenskie/public/data/status.js
@@ -1,17 +1,5 @@
 var userStatus = {
 
-	"N_5": {
-		"status": "sold"
-	},
-
-	"N_1": {
-		"status": "sold"
-	},
-
-	"N_2": {
-		"status": "sold"
-	},
-
 	"N_1-1": {
 		"text": "Центральная Зона отдыха"
 	},
@@ -42,6 +30,137 @@ var userStatus = {
 
 	"N_1-6": {
 		"text": "Пешеходная аллея"
+	},
+
+	"N_304": {
+		"status": "sold"
+	},
+
+	"N_238": {
+		"status": "sold"
+	},
+
+	"N_237": {
+		"status": "sold"
+	},
+
+	"N_243": {
+		"status": "sold"
+	},
+
+	"N_242": {
+		"status": "sold"
+	},
+
+	"N_15": {
+		"status": "sold"
+	},
+
+	"N_31": {
+		"status": "sold"
+	},
+
+	"N_32": {
+		"status": "sold"
+	},
+
+	"N_33": {
+		"status": "sold"
+	},
+
+	"N_29": {
+		"status": "sold"
+	},
+
+	"N_30": {
+		"status": "sold"
+	},
+
+	"N_334": {
+		"status": "sold"
+	},
+
+	"N_335": {
+		"status": "sold"
+	},
+
+	"N_336": {
+		"status": "sold"
+	},
+
+	"N_411": {
+		"status": "wait"
+	},
+
+	"N_11": {
+		"status": "wait"
+	},
+
+	"N_229": {
+		"status": "wait"
+	},
+
+	"N_255": {
+		"status": "wait"
+	},
+	"N_249": {
+		"status": "wait"
+	},
+
+	"N_271": {
+		"status": "wait"
+	},
+
+	"N_160": {
+		"status": "wait"
+	},
+
+	"N_267": {
+		"status": "wait"
+	},
+
+	"N_150": {
+		"status": "wait"
+	},
+
+	"N_170": {
+		"status": "wait"
+	},
+
+	"N_37": {
+		"status": "wait"
+	},
+
+	"N_38": {
+		"status": "wait"
+	},
+
+	"N_39": {
+		"status": "wait"
+	},
+
+	"N_48": {
+		"status": "wait"
+	},
+
+	"N_49": {
+		"status": "wait"
+	},
+
+	"N_50": {
+		"status": "wait"
+	},
+
+	"N_315": {
+		"status": "wait"
+	},
+
+	"N_316": {
+		"status": "wait"
+	},
+
+	"N_317": {
+		"status": "wait"
 	}
 
 };
